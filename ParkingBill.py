c862af98bee91088fd4e388d249afec09e9220a1
z5341468 <yuting.liu4@student.unsw.edu.au>
2023-09-06 13:45:16 +0300

    62%

--- ParkingBill.py
+++ ParkingBill.py
@@ -1,13 +1,23 @@
 from datetime import datetime
 
 def solution(E, L):
+    entry_time = datetime.strptime(E, '%H:%M')
+    exit_time = datetime.strptime(L, '%H:%M')
     
-    en_time = datetime.strftime(E, '%H:%M')
-    ex_time = datetime.strftime(L, '%H:%M')
+    parked_minutes = (exit_time - entry_time).seconds / 60
     
-    parked_minutes = (ex_time - en_time).seconds / 60
-
     parked_full_hours = parked_minutes // 60
     rem_minutes = parked_minutes % 60
 
-    ent_fee = 2
+    parking_bill = 2
+    
+    if parked_full_hours > 0:
+        parking_bill += 3
+        
+    if parked_full_hours > 1:
+        parking_bill += (parked_full_hours - 1) * 4
+        
+    if rem_minutes > 0:
+        parking_bill += 4
+        
+    return int(parking_bill)
