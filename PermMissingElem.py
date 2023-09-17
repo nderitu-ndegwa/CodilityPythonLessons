11034c0a647f3f7ecb63ec96b68ecf185ae74790
nderitu-ndegwa <nderitundegwa10@gmail.com>
2023-09-18 01:54:59 +0300

    4/21

---
+++ PermMissingElem.py
@@ -0,0 +1,42 @@
+'''
+TASK:
+
+An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+
+Your goal is to find that missing element.
+
+Write a function:
+
+    def solution(A)
+
+that, given an array A, returns the value of the missing element.
+
+For example, given array A such that:
+  A[0] = 2
+  A[1] = 3
+  A[2] = 1
+  A[3] = 5
+
+the function should return 4, as it is the missing element.
+
+Write an efficient algorithm for the following assumptions:
+
+        N is an integer within the range [0..100,000];
+        the elements of A are all distinct;
+        each element of array A is an integer within the range [1..(N + 1)].
+'''
+
+
+
+#Solution
+
+def solution(A):
+    sorted = A.sort()
+    n = len(A) + 1
+    hash_set = set(A)
+
+    for i in range(1, n):
+        if i not in hash_set:
+            return i
+
+    return n    
