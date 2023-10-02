d498f89a04084431424746724f488b24970e6e13
nderitu-ndegwa <nderitundegwa10@gmail.com>
2023-10-02 03:37:43 +0300

    9/21 v2

--- MaxCounters.py
+++ MaxCounters.py
@@ -29,6 +29,33 @@ the values of the counters after each consecutive operation will be:
     (3, 2, 2, 3, 2)
     (3, 2, 2, 4, 2)
     
+The goal is to calculate the value of every counter after all operations.
+
+Write a function:
+
+    def solution(N, A)
+
+that, given an integer N and a non-empty array A consisting of M integers, returns a sequence of integers representing the values of the counters.
+
+Result array should be returned as an array of integers.
+
+For example, given:
+    A[0] = 3
+    A[1] = 4
+    A[2] = 4
+    A[3] = 6
+    A[4] = 1
+    A[5] = 4
+    A[6] = 4
+
+the function should return [3, 2, 2, 4, 2], as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+        N and M are integers within the range [1..100,000];
+        each element of array A is an integer within the range [1..N + 1].
+
+
 '''
 
 def solution(N, A):
