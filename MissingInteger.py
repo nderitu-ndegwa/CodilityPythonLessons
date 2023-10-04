5723eebcd6b1027b8f781d380d1586af52916f33
nderitu-ndegwa <nderitundegwa10@gmail.com>
2023-10-04 11:06:15 +0300

    9/21 v2

--- MissingInteger.py
+++ MissingInteger.py
@@ -1,11 +1,34 @@
+'''
+
+This is a demo task.
+
+Write a function:
+
+    def solution(A)
+
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+Given A = [1, 2, 3], the function should return 4.
+
+Given A = [−1, −3], the function should return 1.
+
+Write an efficient algorithm for the following assumptions:
+
+        N is an integer within the range [1..100,000];
+        each element of array A is an integer within the range [−1,000,000..1,000,000].
+
+'''
+
 def solution(A):
-    A.sort()
-    if A[-1] <= 0:
-        return 1
-    elif A[0] > 1:
-        return 1
-    else:
-        for i in range(len(A)-1):
-            if A[i] > 0 and A[i+1] > 0 and A[i+1] - A[i] > 1:
-                return A[i] + 1
-        return A[-1] + 1
+    # Removing duplicates
+    unique_positive_numbers = set(x for x in A if x > 0)
+
+    # Checking for the smallest positive integer
+    smallest_positive = 1
+    while smallest_positive in unique_positive_numbers:
+        smallest_positive += 1
+
+    return smallest_positive
+
